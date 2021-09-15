90daf3297542c740d934b98ded8304680c56b1e0
Siddhartha Pratap Singh <sidps1112@gmail.com>
2021-09-15 12:40:05 +0530

    Add falloff to map shader

--- shaders/map_2d.fs
+++ shaders/map_2d.fs
@@ -60,7 +60,8 @@ int[512] p=int[512](151, 160, 137, 91, 90, 15, 131, 13, 201, 95, 96, 53, 194, 23
              97, 228, 251, 34, 242, 193, 238, 210, 144, 12, 191, 179, 162, 241, 81, 51, 145, 235, 249, 14, 239,
              107, 49, 192, 214, 31, 181, 199, 106, 157, 184, 84, 204, 176, 115, 121, 50, 45, 127, 4, 150, 254,
              138, 236, 205, 93, 222, 114, 67, 29, 24, 72, 243, 141, 128, 195, 78, 66, 215, 61, 156, 180);
-             
+
+// Returns noise value for position
 float noise(float x, float y, float z)
 {  
     // Find the unit cube that contains the point
@@ -116,11 +117,14 @@ float noise(float x, float y, float z)
 // NOISE_GENERATOR
 //----------------------------------------------------
 #define NOISE_FUNCTION get_octave_noise
+#define SCALE_FROM_CENTER 1
+#define LEVEL_OF_DETAIL 0
 #define NOISE_SCALE 50
 #define NUMBER_OCTAVES 4
 #define PERSISTANCE 0.5f
 #define LACUNARITY 2.0f
-#define SCALE_FROM_CENTER 1
+#define FALLOFF_CURVE 5
+#define FALLOFF_SHIFT 10
 
 // Generates a random noise between 0.0 and 1.0
 float rand(vec2 pos)
@@ -132,38 +136,60 @@ float rand(vec2 pos)
 float get_noise(vec2 pos)
 {
     pos *= float(TEXTURE_ROWS);
-    pos = vec2(floor(pos.x),floor(pos.y));
+#if LEVEL_OF_DETAIL
+    pos /= float(LEVEL_OF_DETAIL);
+    pos = vec2(floor(pos.x), floor(pos.y));
+    pos *= float(LEVEL_OF_DETAIL);
+#endif
     return rand(pos);
 }
 
+// Gets the perlin noise for point b/w 0.0 and 1.0
 float perlin(vec2 pos)
 {
-    return noise(pos.x,pos.y,0.0f);
+    return noise(pos.x, pos.y, 0.0f);
 }
 
+// Gets the scaled perlin noise for point
 float get_perlin_noise(vec2 pos)
 {
-     float scale = float(NOISE_SCALE);
-     if(scale<=0.0f)
-     {
-         scale+=0.001f;
-     }
-     pos *= float(TEXTURE_ROWS);
-     //pos = vec2(floor(pos.x),floor(pos.y));
-     return perlin(pos/scale);
+    float rows = float(TEXTURE_ROWS);
+    float columns = rows * (iResolution.x / iResolution.y);
+    pos *= rows;
+    float scale = float(NOISE_SCALE);
+    if(scale <= 0.0f)
+    {
+        scale += 0.001f;
+    }
+    
+    float halfX = 0.0f;
+    float halfY = 0.0f;
+#if SCALE_FROM_CENTER
+    halfX = columns / 2.0f;
+    halfY = rows / 2.0f;
+#endif
+
+#if LEVEL_OF_DETAIL
+    pos /= float(LEVEL_OF_DETAIL);
+    pos = vec2(floor(pos.x), floor(pos.y));
+    pos *= float(LEVEL_OF_DETAIL);
+#endif
+     return perlin((pos - vec2(halfX,halfY)) / scale);
 }
 
+// Gets the scaled octave noise for point
 float get_octave_noise(vec2 pos)
 {
     float rows = float(TEXTURE_ROWS);
-    float columns = rows*(iResolution.x/iResolution.y);
+    pos *= rows;
+    float columns = rows*(iResolution.x / iResolution.y);
     float scale = float(NOISE_SCALE);
-    if(scale<=0.0f)
+    if(scale <= 0.0f)
     {
-        scale+=0.001f;
+        scale += 0.001f;
     }
     
-    int octaves=int(NUMBER_OCTAVES);
+    int octaves = int(NUMBER_OCTAVES);
     float lacunarity = max(LACUNARITY, 1.0f);
     float persistence = min(PERSISTANCE, 1.0f);
     
@@ -175,36 +201,57 @@ float get_octave_noise(vec2 pos)
 #endif
 
     float[20] offsets=float[20](
-    0.0f, 0.0f,0.0f, 0.0f,0.0f,
-    0.0f, 0.0f,0.0f, 0.0f,0.0f,
-    0.0f, 0.0f,0.0f, 0.0f,0.0f,
-    0.0f, 0.0f,0.0f, 0.0f,0.0f
+    0.0f, 0.0f, 0.0f, 0.0f, 0.0f,
+    0.0f, 0.0f, 0.0f, 0.0f, 0.0f,
+    0.0f, 0.0f, 0.0f, 0.0f, 0.0f,
+    0.0f, 0.0f, 0.0f, 0.0f, 0.0f
     );
-    for(int i=0;i<octaves*2;i++)
+    for(int i = 0; i < octaves * 2; i++)
     {
-        offsets[i] = rand(vec2(pos.x,pos.y))*0.00f;
+        offsets[i] = rand(vec2(pos.x, pos.y)) * 0.0f;
     }
     
     float amplitude = 1.0f;
     float frequency = 1.0f;
     float noiseVal = 0.0f;
-    pos *= rows;
-    // pos = vec2(floor(pos.x),floor(pos.y));
     
-    vec2 offset =1.05f*vec2(iTime*1.25f,iTime*1.25f);
-    scale+=sin(iTime*0.85f)*5.25f;
+#if LEVEL_OF_DETAIL
+    pos = vec2(floor(pos.x), floor(pos.y));
+    pos /= float(LEVEL_OF_DETAIL);
+#endif
+
+    vec2 offset = 1.05f * vec2(iTime * 1.25f, iTime * 1.25f);
+    scale += sin(iTime * 0.85f) * 5.25f;
     
     for (int i = 0; i < octaves; i++)
     {
-        float sampleX =(((pos.x-halfX) / scale) * frequency)+offset.x+offsets[i*2];
-        float sampleY =(((pos.y-halfY) / scale) * frequency)+offset.y+offsets[(i*2)+1];
-        float noise = (get_perlin_noise(vec2(sampleX,sampleY)) * 2.0f) - 1.0f;
+        float sampleX = (((pos.x-halfX) / scale) * frequency) + offset.x + offsets[i*2];
+        float sampleY = (((pos.y-halfY) / scale) * frequency) + offset.y + offsets[(i*2)+1];
+        float noise = (get_perlin_noise(vec2(sampleX, sampleY)) * 2.0f) - 1.0f;
         noiseVal += noise * amplitude;
         amplitude *= persistence;
         frequency *= lacunarity;
     }   
-    noiseVal=smoothstep(-0.95f,1.1f,noiseVal);
+    noiseVal = smoothstep(-0.95f, 1.1f, noiseVal);
     return noiseVal;
+}
+
+// Evaluates the falloff curve to match the border
+float eval_curve(float val)
+{
+    float a = float(FALLOFF_CURVE);
+    float b = float(FALLOFF_SHIFT);
+    val = pow(val, a) / (pow(val, a) + pow(b - (b * val), a));
+    return val;
+}
+
+// Returns the falloff map for map
+float falloff(vec2 pos)
+{
+    pos.x /= (iResolution.x / iResolution.y);
+    pos = pos * 2.0f - 1.0f;
+    float val = max(abs(pos.x), abs(pos.y));
+    return clamp(eval_curve(val), 0.0f, 1.0f);
 }
 //----------------------------------------------------
 //----------------------------------------------------
@@ -213,34 +260,47 @@ float get_octave_noise(vec2 pos)
 //----------------------------------------------------
 // MAP_SETTINGS
 //----------------------------------------------------
-vec3[8] regions=vec3[8](
-vec3(0.1f, 0.1f, 0.85f), // DARK BLUE
-vec3(0.2f, 0.4f, 1.0f), // OCEAN BLUE
-vec3(0.9f, 0.9f, 0.2f), // SAND YELLOW
-vec3(0.2f, 0.9f, 0.2f), // LIGHT GREEN
-vec3(0.1f, 0.65f, 0.1f), // DARK GREEN
-vec3(0.5f, 0.25f, 0.0f), // LIGHT BROWN
-vec3(0.3f, 0.075f, 0.0f), // DARK BROWN
-vec3(0.85f, 1.0f, 1.0f) // SNOW BLUE
+
+// Color for all regions
+vec3[8] regions = vec3[8](
+vec3(0.15f, 0.15f, 0.75f), // DARK BLUE
+vec3(0.2f, 0.5f, 1.0f), // OCEAN BLUE
+vec3(0.98f, 0.88f, 0.2f), // SAND YELLOW
+vec3(0.2f, 0.8f, 0.2f), // LIGHT GREEN
+vec3(0.2f, 0.60f, 0.2f), // DARK GREEN
+vec3(0.6f, 0.35f, 0.1f), // LIGHT BROWN
+vec3(0.25f, 0.1f, 0.05f), // DARK BROWN
+vec3(0.85f, 0.95f, 1.0f) // SNOW BLUE
 );
 
-float[8] heights=float[8](
+// Start height for all regions
+float[8] heights = float[8](
 0.0f, // DEEP OCEAN
-0.0325f, // OCEAN
+0.025f, // OCEAN
 0.35f, // COAST
-0.375f, // PLAINS
-0.572f, // FORESTS
-0.675f, // HILLS
-0.825f, // MOUNTAINS
-0.95f // SNOW
+0.365f, // PLAINS
+0.565f, // FORESTS
+0.695f, // HILLS
+0.82f, // MOUNTAINS
+0.93f // SNOW
 );
 
+// Gets the Height val for the pixel
+float get_height(vec2 uv)
+{
+   float height = NOISE_FUNCTION(uv);
+   float diff = falloff(uv);
+   height = clamp(height-diff, 0.0f, 1.0f);
+   return height;
+}
+
+// Gets the index of the region height lies in
 int get_region_index(float h)
 {
-    int index=-1;
-    for(int i=0;i<8;i++)
+    int index = -1;
+    for(int i = 0; i < 8; i++)
     {
-        if(h>=heights[i])
+        if(h >= heights[i])
         {
             index++;
         }
@@ -254,16 +314,13 @@ int get_region_index(float h)
 //----------------------------------------------------
 //----------------------------------------------------
 
-
 void mainImage( out vec4 fragColor, in vec2 fragCoord )
 {
     vec2 uv = fragCoord/iResolution.y;
-    
-    float height = NOISE_FUNCTION(uv);
-    vec3 col = regions[get_region_index(height)];
+    vec3 col = regions[get_region_index(get_height(uv))];
 #if GAMMA_CORRECTION
-    col = pow(col, vec3(1.0f/2.2f));
+    col = pow(col, vec3(1.0f / 2.2f));
 #endif
     // Output to screen
-    fragColor = vec4(col,1.0);
+    fragColor = vec4(col, 1.0);
 }
