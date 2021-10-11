161af2da68de2a3a90b84166f51481d41e3c5f9d
Siddhartha Pratap Singh <sidps1112@gmail.com>
2021-10-11 22:09:46 +0530

    Add common functions

---
+++ functions/uvs.fs
@@ -0,0 +1,10 @@
+float yScale = 1.0;
+
+vec2 get_uv(vec2 fragCoord)
+{
+    vec2 uv = fragCoord / iResolution.xy;
+    uv = uv * 2.0 - 1.0;
+    uv *= yScale;
+    uv.x *= (iResolution.x / iResolution.y);
+    return uv;
+}
